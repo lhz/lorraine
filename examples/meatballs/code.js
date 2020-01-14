b30fc71762d5e7b21b99b3f29bb94375cf4a0b11
Lars Haugseth <git@larshaugseth.com>
2020-01-15 00:03:03 +0100

    More optimisations.

--- examples/meatballs/code.js
+++ examples/meatballs/code.js
@@ -1,5 +1,7 @@
 var vmap;
 
+const BALLS = 5;
+
 function init() {
     screen = 0x000;
     vmap = new Array(384);
@@ -13,21 +15,27 @@ function init() {
 
 
 function draw() {
-    var x, y, b, bx, by, dx, dy, c, v;
+    var x, y, b, dx, dy, c, v;
+    var bx = new Array(BALLS);
+    var by = new Array(BALLS);
+
+    for (b = 0; b < BALLS; b++) {
+	bx[b] = 192.0 + 140.0 * cos(0.005*frame*(b + 1) + 0.653*b);
+	by[b] = 144.0 +  80.0 * sin(0.003*frame*(b + 3) - 1.539*b);
+    }
+
     for (y = 0; y < 288; y++) {
 	for (x = 0; x < 384; x++) {
 	    v = 0.0;
-	    for (b = 0; b < 4; b++) {
-		bx = 192.0 + 140.0 * cos(0.007*frame*(b + 1) + 0.653*b);
-		by = 144.0 +  80.0 * sin(0.005*frame*(b + 3) - 1.539*b);
-		v += vmap[Math.abs(floor(x - bx))][Math.abs(floor(y - by))];
+	    for (b = 0; b < BALLS; b++) {
+		v += vmap[Math.abs(floor(x - bx[b]))][Math.abs(floor(y - by[b]))];
 	    }
 	    if (v > 15)
 		v = 15;
 	    c = floor(v);
 	    //if (c < 15 && (v - c) > 0.0 + ((x + y) % 2) / 2.0) c += 1; // Dither
-	    //if (c < 3) c = 0; // small halo
-	    if (c > 2) c = 15; else c = 0; // black & white
+	    if (c < 3) c = 0; // small halo
+	    //if (c > 4) c = 15; else c = 0; // black & white
 	    plot(x, y, 0x111 * c);
 	}
     }
